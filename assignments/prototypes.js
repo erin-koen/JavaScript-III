9eca6f11f4d085bd8520f5dfddb91fc6e64021fc
erin-koen <erin.koen@gmail.com>
2019-01-23 17:56:31 -0500

    got some of the stretch goal done, can't figure out how to decremement healthPoints in greater than 1

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -23,6 +23,7 @@ GameObject.prototype.destroy = function(){
   return 'Object was removed from the game';
 }
 
+
 /*
   === CharacterStats ===
   * healthPoints
@@ -65,8 +66,12 @@ Humanoid.prototype = Object.create(CharacterStats.prototype);
 Humanoid.prototype.greet = function(){
   return `${this.name} offers a greeting in ${this.language}`
 }
+Humanoid.prototype.die = function(){
+  if (this.health <= 0){
+    destroy(this);
+  }
+}
 
-console.log(Humanoid);
 
 
 
@@ -77,9 +82,21 @@ console.log(Humanoid);
 */
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
+// Stretch task: 
+// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
+// * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+// * Create two new objects, one a villain and one a hero and fight it out with methods!
 
+function Villain(attr) {
+  Humanoid.call(this, attr);
+}
 
-  const mage = new Humanoid({
+Villain.prototype = Object.create(Humanoid.prototype);
+Villain.prototype.harm = function(target, amount){
+  target.healthPoints - amount;
+}
+
+const villain1 = new Villain({
     createdAt: new Date(),
     dimensions: {
       length: 2,
@@ -95,7 +112,7 @@ console.log(Humanoid);
     language: 'Common Tongue',
   });
 
-  const swordsman = new Humanoid({
+  const villain2 = new Villain({
     createdAt: new Date(),
     dimensions: {
       length: 2,
@@ -112,36 +129,72 @@ console.log(Humanoid);
     language: 'Common Tongue',
   });
 
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
-    },
-    healthPoints: 10,
-    name: 'Lilith',
-    team: 'Forest Kingdom',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  });
-
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.healthPoints); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.team); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+villain1.harm(villain2, 16);
 
 
-  // Stretch task: 
-  // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
-  // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  // * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+console.log(villain2)
+
+
+// const mage = new Humanoid({
+  //   createdAt: new Date(),
+  //   dimensions: {
+  //     length: 2,
+  //     width: 1,
+  //     height: 1,
+  //   },
+  //   healthPoints: 5,
+  //   name: 'Bruce',
+  //   team: 'Mage Guild',
+  //   weapons: [
+  //     'Staff of Shamalama',
+  //   ],
+  //   language: 'Common Tongue',
+  // });
+
+  // const swordsman = new Humanoid({
+  //   createdAt: new Date(),
+  //   dimensions: {
+  //     length: 2,
+  //     width: 2,
+  //     height: 2,
+  //   },
+  //   healthPoints: 15,
+  //   name: 'Sir Mustachio',
+  //   team: 'The Round Table',
+  //   weapons: [
+  //     'Giant Sword',
+  //     'Shield',
+  //   ],
+  //   language: 'Common Tongue',
+  // });
+
+  // const archer = new Humanoid({
+  //   createdAt: new Date(),
+  //   dimensions: {
+  //     length: 1,
+  //     width: 2,
+  //     height: 4,
+  //   },
+  //   healthPoints: 10,
+  //   name: 'Lilith',
+  //   team: 'Forest Kingdom',
+  //   weapons: [
+  //     'Bow',
+  //     'Dagger',
+  //   ],
+  //   language: 'Elvish',
+  // });
+
+  // console.log(mage.createdAt); // Today's date
+  // console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+  // console.log(swordsman.healthPoints); // 15
+  // console.log(mage.name); // Bruce
+  // console.log(swordsman.team); // The Round Table
+  // console.log(mage.weapons); // Staff of Shamalama
+  // console.log(archer.language); // Elvish
+  // console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+  // console.log(mage.takeDamage()); // Bruce took damage.
+  // console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+
